caea12783f8350b359b83298da8e1859b7c4f2c7
Aragorn-64 <advaysagarkar@gmail.com>
2023-01-22 01:10:19 +0530

    fix(backend): mongodb deprecation warning

--- conf-backend/app.js
+++ conf-backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const session = require('express-session')
+// const session = require('express-session')
 const logger = require('morgan');
 const apiRouter = require('./routes/ApiRoutes');
 
@@ -23,6 +23,7 @@ mongoose.connect(
     }
   }
 )
+mongoose.set('strictQuery', true);
 
 const app = express();
 
@@ -31,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: process.env.SESSION_SECRET}))
+// app.use(session({secret: process.env.SESSION_SECRET}))
 
 app.get('/', (req, res, next) => {
     res.status(200).send('This is just an API, please use the /api route');
--- conf-backend/controllers/authController.js
+++ conf-backend/controllers/authController.js
@@ -3,24 +3,24 @@ exports.login = ((req, res) => {
     pass = String(pass).toLowerCase();
     //less secure form, doesnt need ans to same Q
     if(pass == process.env.MASPASS){
-        req.session.auth="admin";
+        // req.session.auth="admin";
         res.status(200).send("admin")
     }
     else if(pass == process.env.ANS1 || pass == process.env.ANS2){
-        req.session.auth="auth";
+        // req.session.auth="auth";
         res.status(200).send("auto")
     }
     else{
-        req.session.auth="noauth";
+        // req.session.auth="noauth";
         res.status(200).send("noauth")
     }
 })
 
 exports.checkAuth = ((req, res, next) => {
-    if(req.session.auth == "auth" || req.session.auth == "admin"){
-        next();
-    }
-    else{
-        res.status(401).send('Not logged in');
-    }
+    // if(req.session.auth == "auth" || req.session.auth == "admin"){
+    //     next();
+    // }
+    // else{
+    //     res.status(401).send('Not logged in');
+    // }
 })
